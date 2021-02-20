0bff2df29aff20d14cb60b587ed68838dc2cca79
hans aparicio <hans.moreno@enverus.com>
2021-02-20 14:12:33 -0300

    refactor

--- src/app/components/sumar/sumar.component.ts
+++ src/app/components/sumar/sumar.component.ts
@@ -1,31 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-sumar',
   templateUrl: './sumar.component.html',
   styleUrls: ['./sumar.component.css'],
 })
-export class SumarComponent {
-  formulario: FormGroup;
-  constructor(private _fb: FormBuilder) {
-    this.formulario = this._fb.group({
-      cantidades: this._fb.array([]),
-    });
-    this.getControl();
-  }
-  get getCantidad() {
-    return this.formulario.get('cantidades') as FormArray;
-  }
+export class SumarComponent implements OnInit {
+  formArray = new FormArray([]);
+  rows: Array<{ name: string, control: FormControl}> = [];
+  total: number = 0;
+  constructor() { }
+
   add() {
-    const control = this._fb.group({ cantidad: ['1'] });
-    this.getCantidad.push(control);
+    const newControl = new FormControl(0);
+    this.formArray.push(newControl);
+    this.rows.push({ name: `Cantidad ${this.formArray.length}`, control: newControl })
   }
-  cambio: number;
-  getControl() {
-    this.formulario.valueChanges.subscribe((control) => {
-      this.cambio = control;
-      console.log(this.cambio);
-    });
+
+  ngOnInit(): void {
+    this.updateTotalByRowsChanges();
+  }
+
+  private updateTotalByRowsChanges() {
+    this.formArray.valueChanges
+      .subscribe((values: number[]) => this.total =
+        values.reduce((previousValue, currentValue) => (currentValue + previousValue), 0));
   }
 }
